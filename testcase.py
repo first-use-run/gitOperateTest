182c3719dd0c9353e05b27a9723a699d2f8bcef4
zjtest <1327720451@qq.com>
2021-04-20 15:29:09 +0800

    提交测试文件

---
+++ testcase.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+class Testobj():
+    def test02(self):
+        print("第二条测试用例")
+
+def test01():
+    print("第一条测试用例")
+
+
+if __name__ == "__main__":
+    pytest.main(["-s", "-vv", "testcase.py"])
